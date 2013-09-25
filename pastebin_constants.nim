86058c584397e6463a7f0154e550fb5c1b7124a9
achesak <achesak@yahoo.com>
2013-09-25 18:46:21 -0500

    Added constants file.

---
+++ pastebin_constants.nim
@@ -0,0 +1,11 @@
+# Constants.
+
+const
+    PASTE_PUBLIC* = 0
+    PASTE_UNLISTED* = 1
+    PASTE_PRIVATE* = 2
+    EXPIRE_NEVER* = "N"
+    EXPIRE_10_MIN* = "10M"
+    EXPIRE_1_HOUR* = "1H"
+    EXPIRE_1_DAY* = "1D"
+    EXPIRE_1_MONTH* = "1M"
