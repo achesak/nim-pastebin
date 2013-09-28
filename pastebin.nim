21e5587153127d3201c6624963cdf5536f79fd80
achesak <achesak@yahoo.com>
2013-09-28 11:11:32 -0500

    Finished getUserInfo().

--- pastebin.nim
+++ pastebin.nim
@@ -11,6 +11,9 @@ import pastebin_constants
 import httpclient
 import cgi
 import strutils
+import xmldom
+import xmldomparser
+import xmltree
 
 
 proc createPaste*(devKey : string, pasteData : string, pasteName : string = "", pasteFormat : string = "", pastePrivate : int = 0, pasteExpire : string = ""): string = 
@@ -61,12 +64,36 @@ proc createAPIUserKey*(devKey : string, userName : string, userPassword : string
     return response
 
 
+
+
+
+
+
+
+
+
+
+
 proc listUsersPastes*(devKey : string, resultsLimit : int = 50): string = 
     # stuff goes here
 
 
 proc listTrendingPastes*(devKey : string): string = 
     # stuff goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 proc deletePaste*(devKey : string, userKey : string, pasteKey : string): string = 
@@ -76,15 +103,55 @@ proc deletePaste*(devKey : string, userKey : string, pasteKey : string): string 
     var params : string = "api_option=delete&api_dev_key=" & devKey
     params = params & "&api_user_key=" & userKey & "&api_paste_key=" & pasteKey
     
-    # Create the paste.
+    # Delete the paste.
     var response : string = postContent("http://pastebin.com/api/api_post.php", "Content-Type: application/x-www-form-urlencoded;\c\L", params)
     
     # Returns the status message.
     return response
 
 
-#proc getUserInfo*(devKey : string, userKey : string): array[] = 
-#    # stuff goes here
+proc getUserInfo*(devKey : string, userKey : string): array[9, string] = 
+    # Gets user info.
+    
+    # Build the parameters.
+    var params : string = "api_option=userdetails&api_dev_key=" & devKey
+    params = params & "&api_user_key=" & userKey
+    
+    # Create the paste.
+    var response : string = postContent("http://pastebin.com/api/api_post.php", "Content-Type: application/x-www-form-urlencoded;\c\L", params)
+    
+    # This next part is far from ideal, but Nimrod's XML parsing capabilities
+    # are rather sub-par right now, so I have to do this to get around that.
+    # TODO: fix!
+    
+    # Remove the tags.
+    response = response.replace("<user_name>").replace("</user_name>")
+    response = response.replace("<user_format_short>").replace("</user_format_short>")
+    response = response.replace("<user_expiration>").replace("</user_expiration>")
+    response = response.replace("<user_avatar_url>").replace("</user_avatar_url>")
+    response = response.replace("<user_private>").replace("</user_private>")
+    response = response.replace("<user_website>").replace("</user_website>")
+    response = response.replace("<user_email>").replace("</user_email>")
+    response = response.replace("<user_location>").replace("</user_location>")
+    response = response.replace("<user_account_type>").replace("</user_account_type>")
+    
+    # Split into lines.
+    var response_array : seq[string] = response.splitLines()
+    
+    # Create the user info array.
+    var info : array[9, string]
+    info[0] = response_array[1]
+    info[1] = response_array[2]
+    info[2] = response_array[3]
+    info[3] = response_array[4]
+    info[4] = response_array[5]
+    info[5] = response_array[6]
+    info[6] = response_array[7]
+    info[7] = response_array[8]
+    info[8] = response_array[9]
+    
+    # Return the user info.
+    return info    
 
 
 proc getPaste*(pasteKey : string): string = 
