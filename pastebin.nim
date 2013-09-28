dd6494fa2a1a7ccaf59f416445b624cbc195dfe6
achesak <achesak@yahoo.com>
2013-09-28 14:11:23 -0500

    Finished listUserPastes().

--- pastebin.nim
+++ pastebin.nim
@@ -64,6 +64,45 @@ proc createAPIUserKey*(devKey : string, userName : string, userPassword : string
     return response
 
 
+proc listUserPastes*(devKey : string, userKey : string, resultsLimit : int = 50): seq[seq[string]] = 
+    # Lists user's pastes.
+    
+    # Build the parameters.
+    var params : string = "api_option=list&api_dev_key=" & devKey
+    params = params & "&api_user_key=" & userKey & "&api_results_limit=" & intToStr(resultsLimit)
+    
+    # Create the paste.
+    var response : string = postContent("http://pastebin.com/api/api_post.php", "Content-Type: application/x-www-form-urlencoded;\c\L", params)
+    response = "<pastebin>" & response & "</pastebin>"
+    
+    # Parse the XML.
+    var xml : PXmlNode = parseXML(newStringStream(response))
+    
+    # Create the top level array.
+    var pasteArray : array[resultsLimit, seq[string]]
+    
+    # Loop through the list of pastes.
+    for i in 0..(len(xml) - 1):
+        
+        # Get the paste info.
+        var p1 : string = xml[i][0].innerText
+        var p2 : string = xml[i][1].innerText
+        var p3 : string = xml[i][2].innerText
+        var p4 : string = xml[i][3].innerText
+        var p5 : string = xml[i][4].innerText
+        var p6 : string = xml[i][5].innerText
+        var p7 : string = xml[i][6].innerText
+        var p8 : string = xml[i][7].innerText
+        var p9 : string = xml[i][8].innerText
+        var p10 : string = xml[i][9].innerText
+        
+        # Add the paste info to the array.
+        pasteArray[0] = @[p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+    
+    # Return the list of the pastes.
+    return @pasteArray
+
+
 proc deletePaste*(devKey : string, userKey : string, pasteKey : string): string = 
     # Deletes a paste.
     
@@ -78,7 +117,7 @@ proc deletePaste*(devKey : string, userKey : string, pasteKey : string): string 
     return response
 
 
-proc getUserInfo*(devKey : string, userKey : string): array[9, string] = 
+proc getUserInfo*(devKey : string, userKey : string): seq[string] = 
     # Gets user info.
     
     # Build the parameters.
@@ -92,7 +131,7 @@ proc getUserInfo*(devKey : string, userKey : string): array[9, string] =
     var xml : PXmlNode = parseXML(newStringStream(response))
     
     # Return the user info.
-    return [xml[0].innerText, xml[1].innerText, xml[2].innerText, xml[3].innerText, xml[4].innerText, xml[5].innerText, xml[6].innerText, xml[7].innerText, xml[7].innerText]
+    return @[xml[0].innerText, xml[1].innerText, xml[2].innerText, xml[3].innerText, xml[4].innerText, xml[5].innerText, xml[6].innerText, xml[7].innerText, xml[7].innerText]
 
 
 proc getPaste*(pasteKey : string): string = 
