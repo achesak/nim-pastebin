4b7c00287bd3e870441d1b93e16e46d9072b11b1
achesak <achesak@yahoo.com>
2015-01-02 11:25:00 -0600

    Updated to most recent version of Nim.

--- pastebin.nim
+++ pastebin.nim
@@ -1,7 +1,8 @@
 # Pastebin API wrapper.
 
 # Written by Adam Chesak.
-# Code released under the MIT open source license.
+# Released under the MIT open source license.
+
 
 # Import the formats and constants.
 import pastebin_formats
@@ -17,11 +18,13 @@ import streams
 
 
 proc createPaste*(devKey : string, pasteData : string, pasteName : string = "", pasteFormat : string = "", pastePrivate : int = 0, pasteExpire : string = ""): string = 
-    # Creates a new paste.
+    ## Creates a new paste.
+    ##
+    ## ``devKey`` and ``pasteData`` are required, but everything else is optional. Returns either the URL of the paste or an error message.
     
     # Build the parameters.
     var params : string = "api_option=paste&api_dev_key=" & devKey
-    params = params & "&api_paste_code=" & urlEncode(pasteData) & "&api_paste_name=" & urlEncode(pasteName)
+    params = params & "&api_paste_code=" & encodeUrl(pasteData) & "&api_paste_name=" & encodeUrl(pasteName)
     params = params & "&api_paste_private=" & intToStr(pastePrivate)
     # Only add a format if one was specified.
     if pasteFormat != "":
@@ -38,7 +41,9 @@ proc createPaste*(devKey : string, pasteData : string, pasteName : string = "", 
 
 
 proc createPasteFromFile*(devKey : string, fileName: string, pasteName : string = "", pasteFormat : string = "", pastePrivate : int = 0, pasteExpire : string = ""): string = 
-    # Creates a new paste from a file.
+    ## Creates a new paste from a file.
+    ##
+    ## ``devKey`` and ``fileName`` are required, but everything else is optional. Returns either the URL of the paste or an error message.
     
     # Get the contents of the file.
     var contents : string = readAll(open(fileName))
@@ -51,7 +56,9 @@ proc createPasteFromFile*(devKey : string, fileName: string, pasteName : string 
 
 
 proc createAPIUserKey*(devKey : string, userName : string, userPassword : string): string = 
-    # Gets a user session key.
+    ## Creates a user key.
+    ##
+    ## All parameters are required. Returns either the user key or an error message.
     
     # Build the parameters.
     var params : string = "api_dev_key=" & devKey
@@ -65,7 +72,21 @@ proc createAPIUserKey*(devKey : string, userName : string, userPassword : string
 
 
 proc listUserPastes*(devKey : string, userKey : string, resultsLimit : int = 50): seq[seq[string]] = 
-    # Lists user's pastes.
+    ## Lists a user's pastes.
+    ##
+    ## All parameters are required except for ``resultsLimit``.
+    ## 
+    ## Returns a sequence of sequences of strings, with the information at the folling indicies:
+    ## * 0 - paste key
+    ## * 1 - date created
+    ## * 2 - title
+    ## * 3 - size
+    ## * 4 - expiration date
+    ## * 5 - privacy setting
+    ## * 6 - long format
+    ## * 7 - short format
+    ## * 8 - paste URL
+    ## * 9 - number of hits
     
     # Build the parameters.
     var params : string = "api_option=list&api_dev_key=" & devKey
@@ -76,7 +97,7 @@ proc listUserPastes*(devKey : string, userKey : string, resultsLimit : int = 50)
     response = "<pastebin>" & response & "</pastebin>"
     
     # Parse the XML.
-    var xml : PXmlNode = parseXML(newStringStream(response))
+    var xml : XmlNode = parseXML(newStringStream(response))
     
     # Create the top level array.
     var pasteArray : array[resultsLimit, seq[string]]
@@ -104,7 +125,9 @@ proc listUserPastes*(devKey : string, userKey : string, resultsLimit : int = 50)
 
 
 proc listTrendingPastes*(devKey : string): seq[seq[string]] = 
-    # Lists trending pastes.
+    ## Lists the top 18 trending pastes.
+    ##
+    ## Parameter is required. Returns a sequence of sequences of strings, in the same format as the return value of ``listUserPastes()``.
     
     # Build the parameters.
     var params : string = "api_option=trends&api_dev_key=" & devKey
@@ -114,7 +137,7 @@ proc listTrendingPastes*(devKey : string): seq[seq[string]] =
     response = "<pastebin>" & response & "</pastebin>"
     
     # Parse the XML.
-    var xml : PXmlNode = parseXML(newStringStream(response))
+    var xml : XmlNode = parseXML(newStringStream(response))
     
     # Create the top level array.
     var pasteArray : array[18, seq[string]]
@@ -142,7 +165,9 @@ proc listTrendingPastes*(devKey : string): seq[seq[string]] =
 
 
 proc deletePaste*(devKey : string, userKey : string, pasteKey : string): string = 
-    # Deletes a paste.
+    ## Deletes a paste.
+    ##
+    ## All parameters are required. Returns a status message.
     
     # Build the parameters.
     var params : string = "api_option=delete&api_dev_key=" & devKey
@@ -156,7 +181,20 @@ proc deletePaste*(devKey : string, userKey : string, pasteKey : string): string 
 
 
 proc getUserInfo*(devKey : string, userKey : string): seq[string] = 
-    # Gets user info.
+    ## Gets info about a user.
+    ##
+    ## All parameters are required.
+    ##
+    ## Returns a sequence with the information at the following indices:
+    ## * 0 - user name
+    ## * 1 - short format
+    ## * 2 - user expiration date
+    ## * 3 - avatar URL
+    ## * 4 - privacy setting
+    ## * 5 - website
+    ## * 6 - email
+    ## * 7 - location
+    ## * 8 - account type
     
     # Build the parameters.
     var params : string = "api_option=userdetails&api_dev_key=" & devKey
@@ -166,14 +204,16 @@ proc getUserInfo*(devKey : string, userKey : string): seq[string] =
     var response : string = postContent("http://pastebin.com/api/api_post.php", "Content-Type: application/x-www-form-urlencoded;\c\L", params)
     
     # Parse the XML.
-    var xml : PXmlNode = parseXML(newStringStream(response))
+    var xml : XmlNode = parseXML(newStringStream(response))
     
     # Return the user info.
     return @[xml[0].innerText, xml[1].innerText, xml[2].innerText, xml[3].innerText, xml[4].innerText, xml[5].innerText, xml[6].innerText, xml[7].innerText, xml[7].innerText]
 
 
 proc getPaste*(pasteKey : string): string = 
-    # Gets a paste.
+    ## Gets a paste.
+    ##
+    ## Parameter is required. Returns the contents of the paste.
     
     # Get the data.
     var data : string = getContent("http://pastebin.com/raw.php?i=" & pasteKey)
@@ -183,7 +223,9 @@ proc getPaste*(pasteKey : string): string =
 
 
 proc getPasteToFile*(pasteKey : string, fileName : string): string = 
-    # Gets a paste and stores it in a file.
+    ## Gets a paste and writes it to a file.
+    ##
+    ## All parameters are required. Returns the contents of the paste.
     
     # Get the data.
     var data : string = getContent("http://pastebin.com/raw.php?i=" & pasteKey)
